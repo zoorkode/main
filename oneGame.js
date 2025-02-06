cbe47a0c1f2e7c65a39ffeb198c38292e842dc96
zoorkode <zoorquota@gmail.com>
2025-02-06 19:39:54 +0700

    Create oneGame.js

---
+++ oneGame.js
@@ -0,0 +1,27 @@
+var api = 'KoboStore';
+ function cekNick() {
+    var id = $('#id').val();
+    if (id === "") {
+     $('#nick').text('');
+     $('#nickplayer').text('');
+
+    }
+   
+    $.ajax({
+     method: "GET",
+     url: "https://payday.my.id/trueid/game/" + games + "?id=" + id + "&key=" + api,
+         beforeSend: function () {
+     $('#nick').html('<i class="fas fa-spinner"></i> Sedang Mengecek...');
+     $('#theend').show()
+    },
+    success: function (response) {
+     if (response.hasOwnProperty('error_msg')) {
+      $('#nick').text('Tidak Ditemukan');
+      $('#theend').hide()
+     } else {
+      $('#nick').text(response.nickname);
+      $('#theend').show()
+     }
+    }
+   });
+}
